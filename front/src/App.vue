5f78392742447a16e6b7517d9ca4e8b26bcb224e
YU <yces4199600@gmail.com>
2024-05-03 17:59:56 +0800

    0503

---
+++ front/src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="text-center bg-gray-100 p-5">
+    <p class="text-6xl text-red-700">Hello!</p>
+    <h1 class="text-4xl text-green-500">Vite + TailwindCSS</h1>
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
